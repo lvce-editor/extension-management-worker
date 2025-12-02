9bddc0949a535b3f8c8aca4bd5e4fe3671f5657d
Le Vivilet <levivilet@pm.me>
2025-12-03 00:06:46 +0100

    feature: add runtime status

---
+++ packages/extension-management-worker/src/parts/RuntimeStatus/RuntimeStatus.ts
@@ -0,0 +1,11 @@
+export interface RuntimeStatus {
+  readonly activationEndTime: number
+  readonly activationEvent: string
+  readonly activationStartTime: number
+  readonly activationTime: number
+  readonly id: string
+  readonly importEndTime: number
+  readonly importStartTime: number
+  readonly importTime: number
+  readonly status: number
+}

5be12626f4e58f57cc21f2874e2f5745253ecf03
Le Vivilet <levivilet@pm.me>
2025-12-03 00:08:42 +0100

    feature: add extensions cache

---
+++ packages/extension-management-worker/src/parts/ExtensionsCache/ExtensionsCache.ts
@@ -0,0 +1,19 @@
+const cache = Object.create(null)
+
+const id = 1
+
+export const get = () => {
+  return cache[id]
+}
+
+export const has = () => {
+  return id in cache
+}
+
+export const set = (value) => {
+  cache[id] = value
+}
+
+export const clear = () => {
+  delete cache[id]
+}

0111797c67667eb8db09a8e1089960ff410e51f1
Le Vivilet <levivilet@pm.me>
2025-12-03 00:05:36 +0100

    feature: add uninstall (#3)

--- packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,11 @@
 import { disableExtension } from '../DisableExtension/DisableExtension.ts'
 import { enableExtension } from '../EnableExtension/EnableExtension.ts'
 import { installExtension } from '../InstallExtension/InstallExtension.ts'
+import { uninstallExtension } from '../UninstallExtension/UninstallExtension.ts'
 
 export const commandMap = {
   'Extensions.enable': enableExtension,
   'Extensions.disable': disableExtension,
   'Extensions.install': installExtension,
+  'Extensions.uninstall': uninstallExtension,
 }

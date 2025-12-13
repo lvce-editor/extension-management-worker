29219a2171da939829b618e8437eb4d0570837ab
Le Vivilet <levivilet@pm.me>
2025-12-13 23:57:58 +0100

    feature: create rpc in initialize (#20)

--- packages/extension-management-worker/src/parts/Initialize/Initialize.ts
+++ packages/extension-management-worker/src/parts/Initialize/Initialize.ts
@@ -1,3 +1,17 @@
-export const initialize = async () => {
+import { PlatformType } from '@lvce-editor/constants'
+import { WebSocketRpcParent2 } from '@lvce-editor/rpc'
+import { SharedProcess } from '@lvce-editor/rpc-registry'
+import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
+
+export const initialize = async (platform: number) => {
   // TODO create connection to shared process
+  if (platform === PlatformType.Remote) {
+    const rpc = await WebSocketRpcParent2.create({
+      commandMap: CommandMapRef.commandMapRef,
+      type: 'shared-process',
+    })
+    SharedProcess.set(rpc)
+  } else if (platform === PlatformType.Electron) {
+    // TODO messageport rpc
+  }
 }

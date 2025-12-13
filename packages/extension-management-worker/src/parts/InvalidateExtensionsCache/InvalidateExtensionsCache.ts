bac3d72fee4679b9007dde320b27f2c596e36201
Le Vivilet <levivilet@pm.me>
2025-12-13 23:47:47 +0100

    feature: update command id (#18)

--- packages/extension-management-worker/src/parts/InvalidateExtensionsCache/InvalidateExtensionsCache.ts
+++ packages/extension-management-worker/src/parts/InvalidateExtensionsCache/InvalidateExtensionsCache.ts
@@ -1,5 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const invalidateExtensionsCache = async () => {
-  await RendererWorker.invoke('Extensions.invalidateExtensionsCache')
+  await RendererWorker.invoke('ExtensionManagement.invalidateExtensionsCache')
 }

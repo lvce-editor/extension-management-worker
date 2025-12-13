6abc22e567390edce7b8a8f497de52bc8c78f29b
Le Vivilet <levivilet@pm.me>
2025-12-13 22:18:17 +0100

    lint

--- packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
@@ -7,10 +7,7 @@ import { uninstallExtension } from '../UninstallExtension/UninstallExtension.ts'
 export const commandMap = {
   'Extensions.disable': disableExtension,
   'Extensions.enable': enableExtension,
-<<<<<<< HEAD
   'Extensions.handleMessagePort': handleMessagePort,
-=======
->>>>>>> origin/main
   'Extensions.install': installExtension,
   'Extensions.uninstall': uninstallExtension,
 }

851d62b555eb88d275b08a1e524f984adfdf5900
Le Vivilet <levivilet@pm.me>
2025-12-13 22:35:19 +0100

    feature: add getExtension (#13)

--- packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,6 @@
 import { disableExtension } from '../DisableExtension/DisableExtension.ts'
 import { enableExtension } from '../EnableExtension/EnableExtension.ts'
+import { getExtension } from '../GetExtension/GetExtension.ts'
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 import { installExtension } from '../InstallExtension/InstallExtension.ts'
 import { uninstallExtension } from '../UninstallExtension/UninstallExtension.ts'
@@ -7,6 +8,7 @@ import { uninstallExtension } from '../UninstallExtension/UninstallExtension.ts'
 export const commandMap = {
   'Extensions.disable': disableExtension,
   'Extensions.enable': enableExtension,
+  'Extensions.getExtension': getExtension,
   'Extensions.handleMessagePort': handleMessagePort,
   'Extensions.install': installExtension,
   'Extensions.uninstall': uninstallExtension,

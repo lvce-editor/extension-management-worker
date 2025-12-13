5f9c78ee64652524e73963275498aaba1b3d6055
Le Vivilet <levivilet@pm.me>
2025-12-13 22:30:42 +0100

    feature: add handleMessagePort (#10)

--- packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,11 +1,13 @@
 import { disableExtension } from '../DisableExtension/DisableExtension.ts'
 import { enableExtension } from '../EnableExtension/EnableExtension.ts'
+import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 import { installExtension } from '../InstallExtension/InstallExtension.ts'
 import { uninstallExtension } from '../UninstallExtension/UninstallExtension.ts'
 
 export const commandMap = {
   'Extensions.disable': disableExtension,
   'Extensions.enable': enableExtension,
+  'Extensions.handleMessagePort': handleMessagePort,
   'Extensions.install': installExtension,
   'Extensions.uninstall': uninstallExtension,
 }

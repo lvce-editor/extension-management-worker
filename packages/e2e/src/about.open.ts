5766dca44d5e1ad107d5ee48a6124ff4529844a7
Le Vivilet <levivilet@pm.me>
2025-12-13 22:10:26 +0100

    lint

--- packages/e2e/src/about.open.ts
+++ packages/e2e/src/about.open.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'about.open'
 
-export const test: Test = async ({ Locator, expect, About }) => {
+export const test: Test = async ({ About, expect, Locator }) => {
   await About.show()
   const dialogContent = Locator('.DialogContent')
   await expect(dialogContent).toBeVisible()
--- packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
@@ -4,8 +4,8 @@ import { installExtension } from '../InstallExtension/InstallExtension.ts'
 import { uninstallExtension } from '../UninstallExtension/UninstallExtension.ts'
 
 export const commandMap = {
-  'Extensions.enable': enableExtension,
   'Extensions.disable': disableExtension,
+  'Extensions.enable': enableExtension,
   'Extensions.install': installExtension,
   'Extensions.uninstall': uninstallExtension,
 }

3d8a6107b0392b543d3d748f0488ca18a3dc9bc4
Le Vivilet <levivilet@pm.me>
2025-12-14 11:25:15 +0100

    feature: set platform in initialize (#26)

--- packages/extension-management-worker/src/parts/State/State.ts
+++ packages/extension-management-worker/src/parts/State/State.ts
@@ -1,14 +1,21 @@
 export interface State {
   readonly disabledIds: readonly string[]
+  readonly platform: number
 }
 
 let state: State = {
   disabledIds: [],
+  platform: 0,
 }
 
 export const set = (newState: State): void => {
   state = newState
 }
+export const update = (newState: Partial<State>): void => {
+  const fullNewState = { ...state, ...newState }
+  state = fullNewState
+}
+
 export const get = (): State => {
   return state
 }

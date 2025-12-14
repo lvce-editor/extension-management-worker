01b210a85de0b033a85d4fa237ef8fba174cadbb
Le Vivilet <levivilet@pm.me>
2025-12-14 11:35:32 +0100

    feature: write disabled extensions into disabledExensionsJsonPath

--- packages/extension-management-worker/src/parts/EnableExtension/EnableExtension.ts
+++ packages/extension-management-worker/src/parts/EnableExtension/EnableExtension.ts
@@ -1,23 +1,32 @@
-import { SharedProcess } from '@lvce-editor/rpc-registry'
-import { invalidateExtensionsCache } from '../InvalidateExtensionsCache/InvalidateExtensionsCache.ts'
+import { PlatformType } from '@lvce-editor/constants'
+import { FileSystemWorker, RendererWorker } from '@lvce-editor/rpc-registry'
 import * as State from '../State/State.ts'
 
 export const enableExtension = async (id: string, isTest: boolean): Promise<unknown> => {
   const oldState = State.get()
-
-  try {
-    if (isTest) {
-      const newState: State.State = {
-        ...oldState,
-        disabledIds: oldState.disabledIds.filter((existing) => existing !== id),
-      }
-      State.set(newState)
-    } else {
-      await SharedProcess.invoke('ExtensionManagement.enable', id)
+  const { platform } = oldState
+  if (platform === PlatformType.Remote || platform === PlatformType.Electron) {
+    const disabledExtensionsJsonPath = await RendererWorker.invoke('PlatformPaths.getBuiltinExtensionsJsonPath')
+    const exists = await FileSystemWorker.exists(disabledExtensionsJsonPath)
+    if (!exists) {
+      return undefined
     }
-    await invalidateExtensionsCache()
-    return undefined
-  } catch (error) {
-    return error
+    const content = await FileSystemWorker.readFile(disabledExtensionsJsonPath)
+    const parsed = JSON.parse(content)
+    const oldDisabled = parsed.disabledExtensions || []
+    const newDisabled = oldDisabled.filter((item: any) => item !== id)
+    const newData = {
+      disabledExtensions: newDisabled,
+    }
+    const newContent = JSON.stringify(newData, null, 2) + '\n'
+    await FileSystemWorker.writeFile(disabledExtensionsJsonPath, newContent)
   }
+  if (isTest) {
+    const newState: State.State = {
+      ...oldState,
+      disabledIds: oldState.disabledIds.filter((existing) => existing !== id),
+    }
+    State.set(newState)
+  }
+  return undefined
 }

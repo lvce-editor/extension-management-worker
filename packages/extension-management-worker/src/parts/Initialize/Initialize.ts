fb772b860bd0f42fbb5d4524733fa5aedfabb4d5
Le Vivilet <levivilet@pm.me>
2025-12-14 11:31:28 +0100

    feature: add file system worker

--- packages/extension-management-worker/src/parts/Initialize/Initialize.ts
+++ packages/extension-management-worker/src/parts/Initialize/Initialize.ts
@@ -1,7 +1,8 @@
+import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 import { initializeSharedProcess } from '../InitializeSharedProcess/InitializeSharedProcess.ts'
 import * as State from '../State/State.ts'
 
 export const initialize = async (platform: number) => {
   State.update({ platform })
-  await initializeSharedProcess(platform)
+  await Promise.all([initializeFileSystemWorker(), initializeSharedProcess(platform)])
 }

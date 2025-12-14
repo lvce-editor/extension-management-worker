1973cb0e99e4111dfb4c6d69aadb08bb43fe7a8d
Le Vivilet <levivilet@pm.me>
2025-12-14 11:24:29 +0100

    feature: set platform in initialize

--- packages/extension-management-worker/src/parts/Initialize/Initialize.ts
+++ packages/extension-management-worker/src/parts/Initialize/Initialize.ts
@@ -1,5 +1,7 @@
 import { initializeSharedProcess } from '../InitializeSharedProcess/InitializeSharedProcess.ts'
+import * as State from '../State/State.ts'
 
 export const initialize = async (platform: number) => {
+  State.update({ platform })
   await initializeSharedProcess(platform)
 }

7c9afb41029683cb30d5803c426920748042a7ae
Le Vivilet <levivilet@pm.me>
2025-12-14 11:27:46 +0100

    feature: add getAllExtensions

---
+++ packages/extension-management-worker/src/parts/GetExtensions/GetExtensions.ts
@@ -0,0 +1,11 @@
+import { PlatformType } from '@lvce-editor/constants'
+import { SharedProcess } from '@lvce-editor/rpc-registry'
+import { get } from '../State/State.ts'
+
+export const getAllExtensions = async () => {
+  const state = get()
+  if (state.platform === PlatformType.Web) {
+    return []
+  }
+  return SharedProcess.invoke(/* ExtensionManagement.getAllExtensions */ 'ExtensionManagement.getAllExtensions')
+}

b416776a7d87eb48311b197c7b4e198966294f7e
Le Vivilet <levivilet@pm.me>
2025-12-15 19:15:51 +0100

    feature: set rpc infos in activate

--- packages/extension-management-worker/src/parts/ActivateExtension3/ActivateExtension3.ts
+++ packages/extension-management-worker/src/parts/ActivateExtension3/ActivateExtension3.ts
@@ -1,7 +1,9 @@
 import { activateExtension2 } from '../ActivateExtension2/ActivateExtension2.ts'
+import * as HandleRpcInfos from '../HandleRpcInfos/HandleRpcInfos.ts'
 import { importExtension } from '../ImportExtension/ImportExtension.ts'
 
-export const activateExtension3 = async (extension: any, absolutePath: string, activationEvent: string) => {
+export const activateExtension3 = async (extension: any, absolutePath: string, activationEvent: string, platform: number) => {
+  HandleRpcInfos.handleRpcInfos(extension, platform)
   const extensionId = extension.id
   await importExtension(extensionId, absolutePath, activationEvent)
   await activateExtension2(extensionId, extension, absolutePath)

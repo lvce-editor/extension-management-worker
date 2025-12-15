ca4e9fa0db12bb8d072f2823a25a64e8ee44d792
Le Vivilet <levivilet@pm.me>
2025-12-15 16:51:40 +0100

    feature: add color theme css

---
+++ packages/extension-management-worker/src/parts/GetColorThemeCssCachedNoop/GetColorThemeCssCachedNoop.ts
@@ -0,0 +1,7 @@
+export const get = (colorThemeId: any) => {
+  return ''
+}
+
+export const set = (colorThemeId: any, data: any) => {
+  //  noop
+}
---
+++ packages/extension-management-worker/src/parts/Logger/Logger.ts
@@ -0,0 +1,12 @@
+export const info = (...args) => {
+  // eslint-disable-next-line no-console
+  console.info(...args)
+}
+
+export const warn = (...args) => {
+  console.warn(...args)
+}
+
+export const error = (...args) => {
+  console.error(...args)
+}

41e77f8ba8f86b52ff212f1472db408f9cc2fc90
Le Vivilet <levivilet@pm.me>
2025-12-15 17:57:03 +0100

    pass

--- packages/extension-management-worker/src/parts/Languages/Languages.ts
+++ packages/extension-management-worker/src/parts/Languages/Languages.ts
@@ -2,10 +2,10 @@ import { VError } from '@lvce-editor/verror'
 import * as GetExtensions from '../GetExtensions/GetExtensions.ts'
 import * as GetLanguagesFromExtension from '../GetLanguagesFromExtension/GetLanguagesFromExtension.ts'
 
-export const getLanguages = async () => {
+export const getLanguages = async (platform: number) => {
   try {
     const extensions = await GetExtensions.getAllExtensions()
-    const languages = extensions.flatMap(GetLanguagesFromExtension.getLanguagesFromExtension)
+    const languages = extensions.flatMap((extension: any) => GetLanguagesFromExtension.getLanguagesFromExtension(extension, platform))
     return languages
   } catch (error) {
     throw new VError(error, 'Failed to load languages')

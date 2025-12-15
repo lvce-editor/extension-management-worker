b6d7259dd77f621da20489220c6ec09ec76249eb
Le Vivilet <levivilet@pm.me>
2025-12-15 17:57:39 +0100

    pass

--- packages/extension-management-worker/src/parts/GetColorThemeJson/GetColorThemeJson.ts
+++ packages/extension-management-worker/src/parts/GetColorThemeJson/GetColorThemeJson.ts
@@ -2,9 +2,9 @@ import { PlatformType } from '@lvce-editor/constants'
 import * as GetColorThemeJsonRemote from '../GetColorThemeJsonRemote/GetColorThemeJsonRemote.ts'
 import * as GetColorThemeJsonWeb from '../GetColorThemeJsonWeb/GetColorThemeJsonWeb.ts'
 
-export const getColorThemeJson = (colorThemeId: string, platform: number) => {
+export const getColorThemeJson = (colorThemeId: string, platform: number, assetDir: string) => {
   if (platform === PlatformType.Web) {
-    return GetColorThemeJsonWeb.getColorThemeJson(colorThemeId)
+    return GetColorThemeJsonWeb.getColorThemeJson(colorThemeId, assetDir)
   }
   return GetColorThemeJsonRemote.getColorThemeJson(colorThemeId)
 }

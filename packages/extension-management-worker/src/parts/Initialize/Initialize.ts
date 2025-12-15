fe32d0dc64a79e5af4ed6c7b20a58ad061737de1
Le Vivilet <levivilet@pm.me>
2025-12-15 12:37:33 +0100

    feature: initialize extension host worker (#44)

--- packages/extension-management-worker/src/parts/Initialize/Initialize.ts
+++ packages/extension-management-worker/src/parts/Initialize/Initialize.ts
@@ -1,8 +1,9 @@
+import { initializeExtensionHostWorker } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
 import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 import { initializeSharedProcess } from '../InitializeSharedProcess/InitializeSharedProcess.ts'
 import * as State from '../State/State.ts'
 
 export const initialize = async (platform: number) => {
   State.update({ platform })
-  await Promise.all([initializeFileSystemWorker(), initializeSharedProcess(platform)])
+  await Promise.all([initializeFileSystemWorker(), initializeSharedProcess(platform), initializeExtensionHostWorker()])
 }

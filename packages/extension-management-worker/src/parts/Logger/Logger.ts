aa6a4cacd4ba2e3a49bb9d8ebaa09954abbe971a
Le Vivilet <levivilet@pm.me>
2025-12-15 16:52:03 +0100

    log

--- packages/extension-management-worker/src/parts/Logger/Logger.ts
+++ packages/extension-management-worker/src/parts/Logger/Logger.ts
@@ -1,12 +1,13 @@
-export const info = (...args) => {
+/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
+export const info = (...args: any[]) => {
   // eslint-disable-next-line no-console
   console.info(...args)
 }
 
-export const warn = (...args) => {
+export const warn = (...args: string[]) => {
   console.warn(...args)
 }
 
-export const error = (...args) => {
+export const error = (...args: any[]) => {
   console.error(...args)
 }

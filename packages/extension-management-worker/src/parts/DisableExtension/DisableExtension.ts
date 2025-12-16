a69519c0b4fd882dca1984985a497722990d642c
Le Vivilet <levivilet@pm.me>
2025-12-16 12:11:38 +0100

    feature: pass platform to enable/disable

--- packages/extension-management-worker/src/parts/DisableExtension/DisableExtension.ts
+++ packages/extension-management-worker/src/parts/DisableExtension/DisableExtension.ts
@@ -1,22 +1,7 @@
-import { SharedProcess } from '@lvce-editor/rpc-registry'
-import { invalidateExtensionsCache } from '../InvalidateExtensionsCache/InvalidateExtensionsCache.ts'
+import { disableExtension2 } from '../DisableExtension2/DisableExtension2.ts'
 import * as State from '../State/State.ts'
 
 export const disableExtension = async (id: string, isTest: boolean): Promise<unknown> => {
   const oldState = State.get()
-  try {
-    if (isTest) {
-      const newState: State.State = {
-        ...oldState,
-        disabledIds: [...oldState.disabledIds, id],
-      }
-      State.set(newState)
-    } else {
-      await SharedProcess.invoke('ExtensionManagement.disable', id)
-    }
-    await invalidateExtensionsCache()
-    return undefined
-  } catch (error) {
-    return error
-  }
+  return disableExtension2(id, oldState.platform)
 }

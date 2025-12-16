cab21518f7bb8d6f9f2ec2d8cba5e4896e5b9a8f
Le Vivilet <levivilet@pm.me>
2025-12-16 12:03:30 +0100

    tsc

--- packages/extension-management-worker/src/parts/GetDynamicWebExtensions/GetDynamicWebExtensions.ts
+++ packages/extension-management-worker/src/parts/GetDynamicWebExtensions/GetDynamicWebExtensions.ts
@@ -1,6 +1,4 @@
 import { state } from '../ExtensionMetaState/ExtensionMetaState.ts'
-import * as GetJson from '../GetJson/GetJson.ts'
-import * as WebExtensionsUrl from '../WebExtensionsUrl/WebExtensionsUrl.ts'
 
 export const getDynamicWebExtensions = async (assetDir: string) => {
   return state.webExtensions

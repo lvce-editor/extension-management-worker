4fb568f3726a3cb77dccd90379a5eeeee193dc2c
Le Vivilet <levivilet@pm.me>
2025-12-28 10:15:53 +0100

    feature: add fallback for extension id

--- packages/extension-management-worker/src/parts/ActivateExtension3/ActivateExtension3.ts
+++ packages/extension-management-worker/src/parts/ActivateExtension3/ActivateExtension3.ts
@@ -1,10 +1,11 @@
 import { activateExtension2 } from '../ActivateExtension2/ActivateExtension2.ts'
 import * as HandleRpcInfos from '../HandleRpcInfos/HandleRpcInfos.ts'
 import { importExtension } from '../ImportExtension/ImportExtension.ts'
+import { interExtensionId } from '../InferExtensionId/InferExtensionId.ts'
 
 export const activateExtension3 = async (extension: any, absolutePath: string, activationEvent: string, platform: number) => {
   HandleRpcInfos.handleRpcInfos(extension, platform)
-  const extensionId = extension.id
+  const extensionId = extension.id || interExtensionId(extension.uri)
   await importExtension(extensionId, absolutePath, activationEvent)
   await activateExtension2(extensionId, extension, absolutePath)
 }

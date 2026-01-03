497da010d5c436becf2bb18f44486c81ff74f64c
Le Vivilet <levivilet@pm.me>
2026-01-03 21:53:10 +0100

    args

--- packages/extension-management-worker/src/parts/GetColorThemeNames/GetColorThemeNames.ts
+++ packages/extension-management-worker/src/parts/GetColorThemeNames/GetColorThemeNames.ts
@@ -1,8 +1,8 @@
 import * as GetColorThemeNamesFromExtensions from '../GetColorThemeNamesFromExtensions/GetColorThemeNamesFromExtensions.ts'
 import * as GetExtensions from '../GetExtensions/GetExtensions.ts'
 
-export const getColorThemeNames = async (): Promise<readonly any[]> => {
-  const extensions = await GetExtensions.getAllExtensions()
+export const getColorThemeNames = async (assetDir: string): Promise<readonly any[]> => {
+  const extensions = await GetExtensions.getAllExtensions(assetDir)
   const colorThemeNames = GetColorThemeNamesFromExtensions.getColorThemeNamesFromExtensions(extensions)
   return colorThemeNames
 }

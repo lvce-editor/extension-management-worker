343ef83be177e502c93869bc78bdd04aa4dc0f82
Le Vivilet <levivilet@pm.me>
2026-01-03 15:00:37 +0100

    feature: query web extensions from config

--- packages/extension-management-worker/src/parts/GetExtension/GetExtension.ts
+++ packages/extension-management-worker/src/parts/GetExtension/GetExtension.ts
@@ -1,7 +1,7 @@
 import * as GetExtensions from '../GetExtensions/GetExtensions.ts'
 
-export const getExtension = async (id: string): Promise<any> => {
-  const allExtensions = await GetExtensions.getAllExtensions()
+export const getExtension = async (id: string, assetDir: string): Promise<any> => {
+  const allExtensions = await GetExtensions.getAllExtensions(assetDir)
   for (const extension of allExtensions) {
     if (extension.id === id) {
       return extension
--- packages/extension-management-worker/src/parts/GetExtensions/GetExtensions.ts
+++ packages/extension-management-worker/src/parts/GetExtensions/GetExtensions.ts
@@ -1,13 +1,15 @@
 import { PlatformType } from '@lvce-editor/constants'
 import { SharedProcess } from '@lvce-editor/rpc-registry'
 import { getDynamicWebExtensions } from '../GetDynamicWebExtensions/GetDynamicWebExtensions.ts'
+import { getWebExtensions } from '../GetWebExtensions/GetWebExtensions.ts'
 import { get } from '../State/State.ts'
 
-export const getAllExtensions = async () => {
+export const getAllExtensions = async (assetDir: string) => {
   const state = get()
   const meta = getDynamicWebExtensions()
   if (state.platform === PlatformType.Web) {
-    return [...meta]
+    const webExtensions = await getWebExtensions(assetDir)
+    return [...webExtensions, ...meta]
   }
   const local = await SharedProcess.invoke('ExtensionManagement.getAllExtensions')
   return [...local, ...meta]

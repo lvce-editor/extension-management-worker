d6a1554684f78c4794538ea2ff7bb2a5311c1c85
Le Vivilet <levivilet@pm.me>
2026-01-04 12:46:23 +0100

    validate

--- packages/extension-management-worker/src/parts/GetExtensions/GetExtensions.ts
+++ packages/extension-management-worker/src/parts/GetExtensions/GetExtensions.ts
@@ -1,9 +1,12 @@
+import * as Assert from '@lvce-editor/assert'
 import { PlatformType } from '@lvce-editor/constants'
 import { SharedProcess } from '@lvce-editor/rpc-registry'
 import { getDynamicWebExtensions } from '../GetDynamicWebExtensions/GetDynamicWebExtensions.ts'
 import { getWebExtensions } from '../GetWebExtensions/GetWebExtensions.ts'
 
 export const getAllExtensions = async (assetDir: string, platform: number) => {
+  Assert.string(assetDir)
+  Assert.number(platform)
   const meta = getDynamicWebExtensions()
   if (platform === PlatformType.Web) {
     const webExtensions = await getWebExtensions(assetDir)

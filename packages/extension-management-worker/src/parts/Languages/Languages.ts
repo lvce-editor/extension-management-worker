d548f81df7fbd18136402c3ecf3bdac688008b0d
Le Vivilet <levivilet@pm.me>
2026-01-04 12:39:51 +0100

    feature: validate arguments

--- packages/extension-management-worker/src/parts/Languages/Languages.ts
+++ packages/extension-management-worker/src/parts/Languages/Languages.ts
@@ -1,9 +1,12 @@
+import * as Assert from '@lvce-editor/assert'
 import { VError } from '@lvce-editor/verror'
 import * as GetExtensions from '../GetExtensions/GetExtensions.ts'
 import * as GetLanguagesFromExtension from '../GetLanguagesFromExtension/GetLanguagesFromExtension.ts'
 
 export const getLanguages = async (platform: number, assetDir: string) => {
   try {
+    Assert.number(platform)
+    Assert.string(assetDir)
     const extensions = await GetExtensions.getAllExtensions(assetDir, platform)
     const languages = extensions.flatMap((extension: any) => GetLanguagesFromExtension.getLanguagesFromExtension(extension, platform))
     return languages

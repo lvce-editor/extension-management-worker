148ae827db49a6b611ee92ada964d60aee0c3e7c
Le Vivilet <levivilet@pm.me>
2026-01-04 17:19:54 +0100

    feature: add organizeExtensions

--- packages/extension-management-worker/src/parts/ExtensionMeta/ExtensionMeta.ts
+++ packages/extension-management-worker/src/parts/ExtensionMeta/ExtensionMeta.ts
@@ -1,5 +1,6 @@
 import * as CacheEnabled from '../CacheEnabled/CacheEnabled.ts'
 import * as DoGetExtensions from '../DoGetExtensions/DoGetExtensions.ts'
+import * as ExtensionManifestStatus from '../ExtensionManifestStatus/ExtensionManifestStatus.js'
 import * as GetExtensionsFromCache from '../GetExtensionsFromCache/GetExtensionsFromCache.ts'
 import * as SaveExtensionsInCache from '../SaveExtensionsInCache/SaveExtensionsInCache.ts'
 
@@ -18,4 +19,27 @@ export const getExtensions = async (assetDir: string, platform: number) => {
     return getExtensionsCacheFirst(assetDir, platform)
   }
   return DoGetExtensions.doGetExtensions(assetDir, platform)
+}
+
+// TODO status fulfilled should be handled as resolved
+export const organizeExtensions = (extensions: readonly any[]): any => {
+  const rejected = []
+  const resolved = []
+  for (const extension of extensions) {
+    switch (extension.status) {
+      case ExtensionManifestStatus.Rejected:
+        rejected.push(extension)
+        break
+      case ExtensionManifestStatus.Resolved:
+        resolved.push(extension)
+        break
+      default:
+        resolved.push(extension)
+        break
+    }
+  }
+  return {
+    rejected,
+    resolved,
+  }
 }

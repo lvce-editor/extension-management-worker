e03962c331f07cf9e74f96728717295d0a07b88d
Le Vivilet <levivilet@pm.me>
2026-01-07 16:30:22 +0100

    feature: add assertions (#95)

--- packages/extension-management-worker/src/parts/GetColorThemeNames/GetColorThemeNames.ts
+++ packages/extension-management-worker/src/parts/GetColorThemeNames/GetColorThemeNames.ts
@@ -1,7 +1,10 @@
+import * as Assert from '@lvce-editor/assert'
 import * as GetColorThemeNamesFromExtensions from '../GetColorThemeNamesFromExtensions/GetColorThemeNamesFromExtensions.ts'
 import * as GetExtensions from '../GetExtensions/GetExtensions.ts'
 
 export const getColorThemeNames = async (assetDir: string, platform: number): Promise<readonly any[]> => {
+  Assert.string(assetDir)
+  Assert.number(platform)
   const extensions = await GetExtensions.getAllExtensions(assetDir, platform)
   const colorThemeNames = GetColorThemeNamesFromExtensions.getColorThemeNamesFromExtensions(extensions)
   return colorThemeNames

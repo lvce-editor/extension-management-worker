b95935e48af07a987501d4faa8fd107666194fe3
Le Vivilet <levivilet@pm.me>
2026-05-26 20:30:34 +0200

    refactor: update import to use LazyTransferMessagePortRpcParent in InitializeFileSystemWorker

--- packages/extension-management-worker/src/parts/InitializeFileSystemWorker/InitializeFileSystemWorker.ts
+++ packages/extension-management-worker/src/parts/InitializeFileSystemWorker/InitializeFileSystemWorker.ts
@@ -1,9 +1,9 @@
-import { TransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { FileSystemWorker, RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 
 export const initializeFileSystemWorker = async () => {
-  const rpc = await TransferMessagePortRpcParent.create({
+  const rpc = await LazyTransferMessagePortRpcParent.create({
     commandMap: CommandMapRef.commandMapRef,
     async send(port) {
       await RendererWorker.sendMessagePortToFileSystemWorker(port, 0)

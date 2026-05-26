3b2a4b5e9fe22e62e56d8b54a957548a469b997f
Le Vivilet <levivilet@pm.me>
2026-05-26 20:32:29 +0200

    refactor: update import to use LazyTransferMessagePortRpcParent in InitializeExtensionHostWorker (#112)

--- packages/extension-management-worker/src/parts/InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts
+++ packages/extension-management-worker/src/parts/InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts
@@ -1,9 +1,9 @@
-import { TransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { ExtensionHost, RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 
 export const initializeExtensionHostWorker = async () => {
-  const rpc = await TransferMessagePortRpcParent.create({
+  const rpc = await LazyTransferMessagePortRpcParent.create({
     commandMap: CommandMapRef.commandMapRef,
     async send(port) {
       await RendererWorker.sendMessagePortToExtensionHostWorker(port, 0)

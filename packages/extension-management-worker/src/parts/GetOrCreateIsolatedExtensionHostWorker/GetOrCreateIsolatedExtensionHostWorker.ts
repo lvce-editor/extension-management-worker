144ce6987eab9b30041234352772ee4918bb5dbe
Le Vivilet <levivilet@pm.me>
2026-06-01 16:55:42 +0200

    refactor: update RPC initialization to use TransferMessagePortRpcParent

--- packages/extension-management-worker/src/parts/GetOrCreateIsolatedExtensionHostWorker/GetOrCreateIsolatedExtensionHostWorker.ts
+++ packages/extension-management-worker/src/parts/GetOrCreateIsolatedExtensionHostWorker/GetOrCreateIsolatedExtensionHostWorker.ts
@@ -1,6 +1,5 @@
-import { MessagePortRpcParent, type Rpc } from '@lvce-editor/rpc'
+import { TransferMessagePortRpcParent, type Rpc } from '@lvce-editor/rpc'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
-import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
 import * as IsolatedExtensionHostWorkerState from '../IsolatedExtensionHostWorkerState/IsolatedExtensionHostWorkerState.ts'
 import * as RendererWorker from '../Rpc/Rpc.ts'
 
@@ -9,15 +8,12 @@ export const getOrCreateIsolatedExtensionHostWorker = async (extensionId: string
   if (existingRpc) {
     return existingRpc
   }
-  const { port1, port2 } = GetPortTuple.getPortTuple()
-  const rpcPromise = MessagePortRpcParent.create({
+  const rpc = await TransferMessagePortRpcParent.create({
     commandMap: CommandMapRef.commandMapRef,
-    isMessagePortOpen: true,
-    messagePort: port2,
+    send(port: MessagePort) {
+      return RendererWorker.invokeAndTransfer('LaunchIsolatedExtensionHostWorker.launchIsolatedExtensionHostWorker', port, extensionId)
+    },
   })
-  port2.start()
-  await RendererWorker.invokeAndTransfer('LaunchIsolatedExtensionHostWorker.launchIsolatedExtensionHostWorker', port1, extensionId)
-  const rpc = await rpcPromise
   IsolatedExtensionHostWorkerState.set(extensionId, rpc)
   return rpc
 }

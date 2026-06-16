893aea7edeccfa10b90a0407a08f17d01ba41038
Le Vivilet <levivilet@pm.me>
2026-06-16 19:32:25 +0200

    feature: enhance getRemoteUrl function to return URLs as is for http/https schemes and add tests for getLanguagesFromExtension (#138)

--- packages/extension-management-worker/src/parts/GetRemoteUrl/GetRemoteUrl.ts
+++ packages/extension-management-worker/src/parts/GetRemoteUrl/GetRemoteUrl.ts
@@ -1,4 +1,7 @@
 export const getRemoteUrl = (uri: string): string => {
+  if (uri.startsWith('http://') || uri.startsWith('https://')) {
+    return uri
+  }
   if (uri.startsWith('/')) {
     return `/remote${uri}`
   }

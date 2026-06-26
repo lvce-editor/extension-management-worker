5d54fc19a7822c98fff7b39f96aeabdd78411136
Le Vivilet <levivilet@pm.me>
2026-06-26 18:02:22 +0200

    Invalidate extension cache on extension changes

--- packages/extension-management-worker/src/parts/InstallExtension/InstallExtension.ts
+++ packages/extension-management-worker/src/parts/InstallExtension/InstallExtension.ts
@@ -1,3 +1,6 @@
+import { invalidateExtensionsCache } from '../InvalidateExtensionsCache/InvalidateExtensionsCache.ts'
+
 export const installExtension = async () => {
   // TODO
+  await invalidateExtensionsCache()
 }

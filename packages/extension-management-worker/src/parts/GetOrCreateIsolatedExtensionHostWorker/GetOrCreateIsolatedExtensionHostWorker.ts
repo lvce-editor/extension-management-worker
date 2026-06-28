037ba8dc4e3690fa7573ce0816fcb1bcefb4e83c
Le Vivilet <levivilet@pm.me>
2026-06-29 00:06:36 +0200

    Add isolated extension rpc setup timeout

--- packages/extension-management-worker/src/parts/GetOrCreateIsolatedExtensionHostWorker/GetOrCreateIsolatedExtensionHostWorker.ts
+++ packages/extension-management-worker/src/parts/GetOrCreateIsolatedExtensionHostWorker/GetOrCreateIsolatedExtensionHostWorker.ts
@@ -1,7 +1,9 @@
 import { TransferMessagePortRpcParent, type Rpc } from '@lvce-editor/rpc'
+import * as CancelToken from '../CancelToken/CancelToken.ts'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 import * as IsolatedExtensionHostWorkerState from '../IsolatedExtensionHostWorkerState/IsolatedExtensionHostWorkerState.ts'
 import * as RendererWorker from '../Rpc/Rpc.ts'
+import * as Timeout from '../Timeout/Timeout.ts'
 
 type CreateRpc = (options: {
   readonly commandMap: any
@@ -11,19 +13,38 @@ type CreateRpc = (options: {
 
 type InvokeAndTransfer = typeof RendererWorker.invokeAndTransfer
 
+const rpcConnectionTimeout = 15_000
+
+const rejectAfterTimeout = async (timeout: number, token: any): Promise<never> => {
+  await Timeout.sleep(timeout)
+  if (CancelToken.isCanceled(token)) {
+    return new Promise<never>(() => {})
+  }
+  throw new Error(`Setting up rpc connection failed after ${timeout}ms, likely due to missing activation call`)
+}
+
 export const createIsolatedExtensionHostWorker = async (
   extensionId: string,
   absolutePath: string,
   createRpc: CreateRpc,
   invokeAndTransfer: InvokeAndTransfer,
+  timeout: number = rpcConnectionTimeout,
 ): Promise<Rpc> => {
-  return createRpc({
-    commandMap: CommandMapRef.commandMapRef,
-    isMessagePortOpen: true,
-    send(port: MessagePort) {
-      return invokeAndTransfer('LaunchIsolatedExtensionHostWorker.launchIsolatedExtensionHostWorker', port, extensionId, absolutePath)
-    },
-  })
+  const token = CancelToken.create()
+  try {
+    return await Promise.race([
+      createRpc({
+        commandMap: CommandMapRef.commandMapRef,
+        isMessagePortOpen: true,
+        send(port: MessagePort) {
+          return invokeAndTransfer('LaunchIsolatedExtensionHostWorker.launchIsolatedExtensionHostWorker', port, extensionId, absolutePath)
+        },
+      }),
+      rejectAfterTimeout(timeout, token),
+    ])
+  } finally {
+    CancelToken.cancel(token)
+  }
 }
 
 export const getOrCreateIsolatedExtensionHostWorker = async (extensionId: string, absolutePath: string): Promise<Rpc> => {

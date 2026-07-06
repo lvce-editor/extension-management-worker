87074ead25c2708677a574c402f1b91de2eb0d8e
Le Vivilet <levivilet@pm.me>
2026-07-07 00:07:35 +0200

    [codex] bridge extension keybindings and context (#168)

---
+++ packages/extension-management-worker/test/GetKeyBindings.test.ts
@@ -0,0 +1,49 @@
+import type { DisposableMockRpc } from '@lvce-editor/rpc-registry'
+import { afterEach, expect, test } from '@jest/globals'
+import { SharedProcess } from '@lvce-editor/rpc-registry'
+import * as ExtensionsState from '../src/parts/ExtensionsState/ExtensionsState.ts'
+import { getKeyBindings } from '../src/parts/GetKeyBindings/GetKeyBindings.ts'
+
+const state: {
+  sharedProcess: DisposableMockRpc | undefined
+} = {
+  sharedProcess: undefined,
+}
+
+afterEach(() => {
+  ExtensionsState.reset()
+  state.sharedProcess?.[Symbol.dispose]()
+  state.sharedProcess = undefined
+})
+
+test('returns valid extension keybinding contributions', async () => {
+  state.sharedProcess = SharedProcess.registerMockRpc({
+    'ExtensionManagement.getAllExtensions'() {
+      return [
+        {
+          id: 'sample.extension',
+          keybindings: [
+            {
+              command: 'sample.run',
+              key: 'Ctrl+Enter',
+              when: 'sample.focus',
+            },
+            {
+              command: 1,
+              key: 'Escape',
+            },
+          ],
+        },
+      ]
+    },
+  })
+
+  await expect(getKeyBindings('', 2)).resolves.toEqual([
+    {
+      command: 'sample.run',
+      extensionId: 'sample.extension',
+      key: 'Ctrl+Enter',
+      when: 'sample.focus',
+    },
+  ])
+})

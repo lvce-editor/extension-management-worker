8755fec0460cc157fbeae5241f40377e58274f8d
Le Vivilet <levivilet@pm.me>
2026-07-13 17:13:25 +0200

    fix: show isolated running extensions (#190)

---
+++ packages/extension-management-worker/src/parts/ActivateIsolatedExtension/ActivateIsolatedExtension.ts
@@ -0,0 +1,34 @@
+import * as ExtensionsState from '../ExtensionsState/ExtensionsState.ts'
+import * as GetOrCreateIsolatedExtensionHostWorker from '../GetOrCreateIsolatedExtensionHostWorker/GetOrCreateIsolatedExtensionHostWorker.ts'
+import * as RuntimeStatusType from '../RuntimeStatusType/RuntimeStatusType.ts'
+
+type GetOrCreate = typeof GetOrCreateIsolatedExtensionHostWorker.getOrCreateIsolatedExtensionHostWorker
+
+export const activateIsolatedExtension = async (
+  extensionId: string,
+  absolutePath: string,
+  workerName: string,
+  activationEvent: string,
+  getOrCreate: GetOrCreate = GetOrCreateIsolatedExtensionHostWorker.getOrCreateIsolatedExtensionHostWorker,
+): Promise<void> => {
+  const startTime = performance.now()
+  ExtensionsState.updateRuntimeStatus(extensionId, {
+    activationEvent,
+    activationStartTime: startTime,
+    status: RuntimeStatusType.Activating,
+  })
+  try {
+    await getOrCreate(extensionId, absolutePath, workerName)
+    const endTime = performance.now()
+    ExtensionsState.updateRuntimeStatus(extensionId, {
+      activationEndTime: endTime,
+      activationTime: endTime - startTime,
+      status: RuntimeStatusType.Activated,
+    })
+  } catch (error) {
+    ExtensionsState.updateRuntimeStatus(extensionId, {
+      status: RuntimeStatusType.Error,
+    })
+    throw error
+  }
+}

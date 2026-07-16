dab2a57241428da2c38dc00806e7ab2646b93d2e
Le Vivilet <levivilet@pm.me>
2026-07-16 12:47:53 +0000

    fix: skip hidden status bar refresh

--- packages/extension-management-worker/src/parts/StatusBarHandleChange/StatusBarHandleChange.ts
+++ packages/extension-management-worker/src/parts/StatusBarHandleChange/StatusBarHandleChange.ts
@@ -1,5 +1,9 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const handleChange = async (_id: string): Promise<void> => {
+  const statusBarVisible = await RendererWorker.invoke('Layout.getStatusBarVisible')
+  if (!statusBarVisible) {
+    return
+  }
   await RendererWorker.invoke('StatusBar.handleItemsChanged')
 }

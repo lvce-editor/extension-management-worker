704cd083a86c7e0b790e8441b71925d1343a64d1
Le Vivilet <levivilet@pm.me>
2026-07-18 06:19:53 +0000

    fix: resolve running extension icons

--- packages/extension-management-worker/src/parts/GetRunningExtensions/GetRunningExtensions.ts
+++ packages/extension-management-worker/src/parts/GetRunningExtensions/GetRunningExtensions.ts
@@ -1,9 +1,11 @@
 import * as ExtensionsState from '../ExtensionsState/ExtensionsState.ts'
 import { getAllExtensions } from '../GetExtensions/GetExtensions.ts'
 import { getRunningExtensionsFromState } from '../GetRunningExtensionsFromState/GetRunningExtensionsFromState.ts'
+import { getRuntimeContext } from '../GetRuntimeContext/GetRuntimeContext.ts'
 
 export const getRunningExtensions = async (assetDir: string, platform: number): Promise<readonly any[]> => {
-  const extensions = await getAllExtensions(assetDir, platform)
+  const { assetDir: resolvedAssetDir, platform: resolvedPlatform } = await getRuntimeContext(assetDir, platform)
+  const extensions = await getAllExtensions(resolvedAssetDir, resolvedPlatform)
   const { runtimeStatuses } = ExtensionsState.get()
-  return getRunningExtensionsFromState(extensions, runtimeStatuses)
+  return getRunningExtensionsFromState(extensions, runtimeStatuses, resolvedAssetDir, resolvedPlatform)
 }

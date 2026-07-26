19a06aa295f889f865027115d359371dd5d2c6f2
Le Vivilet <levivilet@pm.me>
2026-07-26 13:38:27 +0000

    Add running extension lifecycle statuses

--- packages/extension-management-worker/src/parts/GetRunningExtensions/GetRunningExtensions.ts
+++ packages/extension-management-worker/src/parts/GetRunningExtensions/GetRunningExtensions.ts
@@ -2,10 +2,12 @@ import * as ExtensionsState from '../ExtensionsState/ExtensionsState.ts'
 import { getAllExtensions } from '../GetExtensions/GetExtensions.ts'
 import { getRunningExtensionsFromState } from '../GetRunningExtensionsFromState/GetRunningExtensionsFromState.ts'
 import { getRuntimeContext } from '../GetRuntimeContext/GetRuntimeContext.ts'
+import { updateTerminatedExtensionStatuses } from '../UpdateTerminatedExtensionStatuses/UpdateTerminatedExtensionStatuses.ts'
 
 export const getRunningExtensions = async (assetDir: string, platform: number): Promise<readonly any[]> => {
   const { assetDir: resolvedAssetDir, platform: resolvedPlatform } = await getRuntimeContext(assetDir, platform)
   const extensions = await getAllExtensions(resolvedAssetDir, resolvedPlatform)
+  await updateTerminatedExtensionStatuses(ExtensionsState.get().runtimeStatuses)
   const { runtimeStatuses } = ExtensionsState.get()
   return getRunningExtensionsFromState(extensions, runtimeStatuses, resolvedAssetDir, resolvedPlatform)
 }
--- packages/extension-management-worker/src/parts/RuntimeStatusType/RuntimeStatusType.ts
+++ packages/extension-management-worker/src/parts/RuntimeStatusType/RuntimeStatusType.ts
@@ -3,3 +3,4 @@ export const Importing = 1
 export const Activating = 2
 export const Activated = 3
 export const Error = 4
+export const Terminated = 5

f31a5cab9e0090718c7b1f72919902a1c5a7ee02
Le Vivilet <levivilet@pm.me>
2026-08-07 11:41:44 +0200

    fix

--- packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-management-worker/src/parts/CommandMap/CommandMap.ts
@@ -180,8 +180,8 @@ export const commandMap: Record<string, (...args: readonly any[]) => any> = {
   'Extensions.showViewContextMenu': ExtensionView.showViewContextMenu,
   'Extensions.uninstall': uninstallExtension,
   'StatusBar.handleChange': StatusBarHandleChange.handleChange,
+  'WebRtc.readMicLevels': WebRtc.readMicLevels,
   'WebRtc.setRemoteDescription': WebRtc.setRemoteDescription,
   'WebRtc.startWebRtcAudioStream': WebRtc.startWebRtcAudioStream,
   'WebRtc.stopWebRtcAudioStream': WebRtc.stopWebRtcAudioStream,
-  'WebRtc.readMicLevels': WebRtc.readMicLevels,
 }

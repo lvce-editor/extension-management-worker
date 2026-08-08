6ca413e022c1379cb78953d193581e030b69cb2a
Le Vivilet <levivilet@pm.me>
2026-08-08 07:08:39 +0000

    fix: implement extension uninstall

--- packages/extension-management-worker/src/parts/UninstallExtension/UninstallExtension.ts
+++ packages/extension-management-worker/src/parts/UninstallExtension/UninstallExtension.ts
@@ -1,6 +1,13 @@
+import { SharedProcess } from '@lvce-editor/rpc-registry'
+import { disposeIsolatedExtensionHostWorker } from '../DisposeIsolatedExtensionHostWorker/DisposeIsolatedExtensionHostWorker.ts'
+import * as ExtensionsState from '../ExtensionsState/ExtensionsState.ts'
 import { invalidateExtensionsCache } from '../InvalidateExtensionsCache/InvalidateExtensionsCache.ts'
 
-export const uninstallExtension = async () => {
-  // TODO
+export const uninstallExtension = async (id: string): Promise<void> => {
+  const removedWebExtension = ExtensionsState.removeWebExtension(id)
+  if (!removedWebExtension) {
+    await SharedProcess.invoke('ExtensionManagement.uninstall', id)
+  }
+  await disposeIsolatedExtensionHostWorker(id)
   await invalidateExtensionsCache()
 }

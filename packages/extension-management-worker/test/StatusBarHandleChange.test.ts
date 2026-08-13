80890182a7e068d98a823f5289da54155e0465ab
Le Vivilet <levivilet@pm.me>
2026-08-13 12:55:37 +0000

    fix: route status bar updates through direct port

--- packages/extension-management-worker/test/StatusBarHandleChange.test.ts
+++ packages/extension-management-worker/test/StatusBarHandleChange.test.ts
@@ -1,16 +1,24 @@
+import type { Rpc } from '@lvce-editor/rpc'
 import { expect, test } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { handleChange } from '../src/parts/StatusBarHandleChange/StatusBarHandleChange.ts'
+import * as StatusBarWorker from '../src/parts/StatusBarWorker/StatusBarWorker.ts'
 
-test('handleChange refreshes the status bar through the renderer', async () => {
-  using mockRpc = RendererWorker.registerMockRpc({
+test('handleChange refreshes the status bar through its direct port', async () => {
+  const invocations: unknown[][] = []
+  StatusBarWorker.set({
+    async invoke(method: string, ...params: readonly unknown[]): Promise<void> {
+      invocations.push([method, ...params])
+    },
+  } as unknown as Rpc)
+  using mockRendererRpc = RendererWorker.registerMockRpc({
     'Layout.getStatusBarVisible': async () => true,
-    'StatusBar.handleItemsChanged': async () => {},
   })
 
   await handleChange('git.checkout')
 
-  expect(mockRpc.invocations).toEqual([['Layout.getStatusBarVisible'], ['StatusBar.handleItemsChanged']])
+  expect(mockRendererRpc.invocations).toEqual([['Layout.getStatusBarVisible']])
+  expect(invocations).toEqual([['StatusBar.handleChange']])
 })
 
 test('handleChange skips the refresh when the status bar is hidden', async () => {

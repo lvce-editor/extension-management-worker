b6b6d2fed1b3e853e5e6609ce3eb68090e12bb0c
Le Vivilet <levivilet@pm.me>
2026-08-20 11:17:02 +0000

    fix: route electron ports to the embeds worker

--- packages/extension-management-worker/src/parts/SendMessagePortToElectron/SendMessagePortToElectron.ts
+++ packages/extension-management-worker/src/parts/SendMessagePortToElectron/SendMessagePortToElectron.ts
@@ -1,5 +1,5 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { RendererWorker, RpcId } from '@lvce-editor/rpc-registry'
 
 export const sendMessagePortToElectron = async (port: MessagePort, initialCommand: string): Promise<void> => {
-  await RendererWorker.invokeAndTransfer('SendMessagePortToElectron.sendMessagePortToElectron', port, initialCommand)
+  await RendererWorker.invokeAndTransfer('SendMessagePortToElectron.sendMessagePortToElectron', port, initialCommand, RpcId.EmbedsWorker)
 }
